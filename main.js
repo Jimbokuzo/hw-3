f961d93a3a6a2c68c65605c713a7f72abb70aa77
grunsky yegor <chopgruny@gmail.com>
2022-02-09 16:44:11 +0200

    45

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
 
 var message = prompt("Введите ваш номер телефона:")
-var isError = false
 var isOk = true
 if (message.length < 1 || message.length < 13 || message.length > 13) {
         console.log(`error`)
-        alert(' длина должна быть 13 символов и поле недолжно быть пустым')
-        isError = true
+        alert('Поле не должно быть пустым и телефон должен быть заполнен в формате +380735070205')
         isOk = false
     }
     if (message[0] < 43 || message[0] > 43) {
         console.log(`error`)
-        alert('Номер телефона должен начинаться на +')
-        isError = true
+        alert('Телефон должен вводться в формате +380735070205')
+
         isOk = false
     }
-    
 for (var char of message.slice(1)) {
     var code = char.charCodeAt()
     if (code < 48 || code > 57) {
         console.log(`error`)
         alert('Номер телефона должен состоять только из цифр')
-        isError = true
+
         isOk = false
         break
     }
     console.log(code)
 }
-if (isError) {
-    alert("Введите корректный номер. Например: +380735070005")
-}
 if (isOk) {
     alert("Спасибо)")
 }
+ 
