2c72cfca2b5f11569f0a963d71c23d7324a71cfe
grunsky yegor <chopgruny@gmail.com>
2022-02-09 16:15:32 +0200

    123

--- main.js
+++ main.js
@@ -2,28 +2,21 @@
 var message = prompt("Введите ваш номер телефона:")
 var isError = false
 var isOk = true
-if(message.length < 1){
-    console.log(`error`)
-    alert(`Поле не должно быть пустым`)
-    isError = true
-    isOk = false
-}
-if (message[0] < 43 || message[0] > 43) {
+if (message.length < 1 || message.length < 13 || message.length > 13) {
+        console.log(`error`)
+        alert(' длина должна быть 13 символов и поле недолжно быть пустым')
+        isError = true
+        isOk = false
+    }
+    if (message[0] < 43 || message[0] > 43) {
         console.log(`error`)
         alert('Номер телефона должен начинаться на +')
         isError = true
         isOk = false
     }
-    if (message.length < 13 || message.length > 13) {
-        console.log(`error`)
-        alert('Номер телефона должен быть длиной 13 символов')
-        isError = true
-        isOk = false
-        
-    }
+    
 for (var char of message.slice(1)) {
     var code = char.charCodeAt()
-    
     if (code < 48 || code > 57) {
         console.log(`error`)
         alert('Номер телефона должен состоять только из цифр')
