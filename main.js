7fba0bb822a75eea591b207f9525af752cafa3a9
grunsky yegor <chopgruny@gmail.com>
2022-02-09 17:08:56 +0200

    12

--- main.js
+++ main.js
@@ -1,32 +1,29 @@
-var message = prompt("Введите ваш номер телефона:");
-var isOk = true;
-
-if (message.length < 13  message.length > 13) {
-  alert(
-    "Поле не должно быть пустым и телефон должен быть заполнен в формате +380735070205"
-  );
-  isOk = false;
-}
-
-if (isOk && message[0] !== 43) {
-  alert("Телефон должен вводться в формате +380735070205");
-
-  isOk = false;
-}
-
-if (isOk === true) {
-  for (var char of message.slice(1)) {
-    var code = char.charCodeAt();
-    if (code < 48  code > 57) {
-      alert("Номер телефона должен состоять только из цифр");
-
-      isOk = false;
-      break;
+var message = prompt("Введите ваш номер телефона:")
+var isOk = true
+if (message.length < 13 || message.length > 13) {
+        console.log("error")
+        alert('Поле не должно быть пустым и телефон должен быть заполнен в формате +380735070205')
+        isOk = false
     }
-    console.log(code);
-  }
-}
+    if(isOk && message[0] !== 43) {
+        console.log("error")
+        alert('Телефон должен вводться в формате +380735070205')
 
+        isOk = false
+    }
+if(isOk === true){
+    for (var char of message.slice(1)) {
+        var code = char.charCodeAt()
+        if (code < 48 || code > 57) {
+            console.log("error")
+            alert('Номер телефона должен состоять только из цифр')
+    
+            isOk = false
+            break
+        }
+        console.log(code)
+    }
+}
 if (isOk) {
-  alert("Спасибо)");
+    alert("Спасибо)")
 }
